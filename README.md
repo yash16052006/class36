3f2cf2720bd7f4b9a0535cef5a655323615b555f
yash16052006 <86788569+yash16052006@users.noreply.github.com>
2021-10-02 14:57:47 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# class36
-speed racer class1
+# C36_SpeedRacer_TeacherActivity
---
+++ js/Player.js
@@ -0,0 +1,10 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+  }
+
+  
+}
